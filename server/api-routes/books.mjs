865e00a5da2fa1c7b46a292532a1d2f4774d6c2d
sasatank <sasa.tankytank@gmail.com>
2023-07-13 21:36:13 +0900

    init

---
+++ server/api-routes/books.mjs
@@ -0,0 +1,35 @@
+import express from "express";
+import { body } from "express-validator";
+import { requestErrorHandler } from "../helpers/helper.mjs";
+import { deleteBook, getAllBooks, getBookById, registBook, updateBook } from "../controllers/books.mjs";
+
+const router = express.Router();
+
+
+
+// /api/books
+router.get("/", requestErrorHandler(getAllBooks));
+
+router.get("/:id", requestErrorHandler(getBookById));
+
+router.post(
+  "/",
+  body("title").notEmpty(),
+  body("description").notEmpty(),
+  body("comment").notEmpty(),
+  body("rating").notEmpty().isInt({ min: 1, max: 5 }),
+  requestErrorHandler(registBook)
+);
+
+router.patch(
+  "/:id",
+  body("title").optional().notEmpty(),
+  body("description").optional().notEmpty(),
+  body("comment").optional().notEmpty(),
+  body("rating").optional().notEmpty().isInt({ min: 1, max: 5 }),
+  requestErrorHandler(updateBook)
+);
+
+router.delete("/:id", requestErrorHandler(deleteBook));
+
+export default router;
---
+++ server/app.mjs
@@ -0,0 +1,44 @@
+import path from "path";
+import express from "express";
+import apiRoutes from "./api-routes/index.mjs";
+import env from "dotenv";
+import "./helpers/db.mjs";
+import cors from "cors";
+
+env.config();
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use('/',express.static('build'));
+app.use(express.json());
+
+
+// app.use(
+//     cors({
+       
+//     })
+// );
+
+
+//API
+app.use('/api',apiRoutes);
+
+app.get('*',function(req,res){
+    const indexHtml = path.resolve('build','index.html');
+    res.sendFile(indexHtml);
+});
+
+app.use(function(req,res){
+    res.status(404).json({ msg: "Page Not Found"})
+})
+
+app.use(function(err,req,res,next){
+    if(res.headersSent){
+        return next(err);
+    }
+    res.status(500).json({msg:"不正なエラーが発生しました。"});
+})
+
+app.listen(port, () => {
+    console.log(`Server Start: http://localhost:${port}`);
+})
